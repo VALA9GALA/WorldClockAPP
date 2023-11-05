6394078fc55cc07f41c294ec22f7a1d24b5c1524
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2023-11-04 23:28:16 -0400

    Worked on <select> dropdown functionality

--- JS/index.js
+++ JS/index.js
@@ -7,15 +7,18 @@ function updateTime() {
   nyDateElement.innerHTML = nyDateTime.format("MMMM D, YYYY");
   nyTimeElement.innerHTML = nyDateTime.format("h:mm:ss[<small>] A[</small>]");
 
-  // Los Angeles
-  let laElement = document.querySelector("#la");
-  let laDateElement = laElement.querySelector(".date");
-  let laTimeElement = laElement.querySelector(".time");
-  let laDateTime = moment().tz("America/Los_Angeles");
-  laDateElement.innerHTML = laDateTime.format("MMMM D, YYYY");
-  laTimeElement.innerHTML = laDateTime.format("h:mm:ss[<small>] A[</small>]");
+  // Giza
 
-  // Mexico City
+  let gizaElement = document.querySelector("#giza");
+  let gizaDateElement = gizaElement.querySelector(".date");
+  let gizaTimeElement = gizaElement.querySelector(".time");
+  let gizaDateTime = moment().tz("Africa/Cairo");
+  gizaDateElement.innerHTML = gizaDateTime.format("MMMM D, YYYY");
+  gizaTimeElement.innerHTML = gizaDateTime.format(
+    "h:mm:ss[<small>] A[</small>]"
+  );
+
+  /*// Mexico City
   let mexicoCityElement = document.querySelector("#mexico-city");
   let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
   let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
@@ -23,8 +26,25 @@ function updateTime() {
   mexicoCityDateElement.innerHTML = mexicoCityDateTime.format("MMMM D, YYYY");
   mexicoCityTimeElement.innerHTML = mexicoCityDateTime.format(
     "h:mm:ss[<small>] A[</small>]"
-  );
+  );*/
 }
 
+function updateCitySelect(event) {
+  event.preventDefault();
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityDateTime = moment().tz(cityTimezone);
+  let firstCityElement = document.querySelector("#selected-city");
+  firstCityElement.innerHTML = `<div id="#selected-city">
+          <h2>${cityName}</h2>
+          <div class="date">${cityDateTime.format("MMMM D, YYYY")}</div>
+          <div class="time">${cityDateTime.format(
+            "h:mm:ss[<small>] A[</small>]"
+          )}</div></div>`;
+}
+
+let citySelectElement = document.querySelector("#city-select");
+citySelectElement.addEventListener("change", updateCitySelect);
+
 updateTime();
-setInterval(updateTime, 1000);
+setInterval(updateTime, updateCitySelect, 1000);
